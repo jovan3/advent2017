d6cb1d74a703b6c585ef1923ca0ed16a910f19ef
Yovan Naumovski <jovan3.14@gmail.com>
2017-12-02 11:40:41 +0200

    add day 2 part 1.

--- src/advent2017/day2.clj
+++ src/advent2017/day2.clj
@@ -1,3 +1,10 @@
-(ns advent2017.day2)
+(ns advent2017.day2
+  (:require [clojure.string :as str]))
 
-(defn day2 [input] input)
+(defn tabnums->list [tabulated_num_string]
+  (map #(Integer/parseInt %) (str/split tabulated_num_string #"\t")))
+
+(defn day2 [input]
+  (let [processed-input (str/split-lines input)
+        row-lists (map tabnums->list processed-input)]
+    (reduce + (map #(- (apply max %) (apply min %)) row-lists))))
