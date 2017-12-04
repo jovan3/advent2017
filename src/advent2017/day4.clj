9337d8c92df19f770680569c04a819334c03167e
Yovan Naumovski <jovan3.14@gmail.com>
2017-12-04 23:08:43 +0200

    add day 4 part 2.

--- src/advent2017/day4.clj
+++ src/advent2017/day4.clj
@@ -4,13 +4,16 @@
 (defn line-words [line]
   (str/split line #" "))
 
-(defn num-lines-repeating-words [input]
+(defn num-lines-repeating-words [input line-function]
   (frequencies (map (fn [n] (some #(not= 1 %) n))
                     (->>
                      (str/split-lines input)
-                     (map line-words)
+                     (map line-function)
                      (map frequencies)
                      (map vals)))))
 
+(defn sorted-letter-words [line-string] (map sort (line-words line-string)))
+
 (defn day4 [input]
-  (println "day 4: " (get (num-lines-repeating-words input) nil)))
+  (println "day 4 part 1: " (get (num-lines-repeating-words input line-words) nil))
+  (println "day 4 part 2: " (get (num-lines-repeating-words input sorted-letter-words) nil)))
