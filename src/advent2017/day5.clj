783fccef55bf2eae28c4588908f00aa09df91a47
Yovan Naumovski <jovan3.14@gmail.com>
2017-12-05 21:09:29 +0200

    add day 5 part 2.

--- src/advent2017/day5.clj
+++ src/advent2017/day5.clj
@@ -1,13 +1,18 @@
 (ns advent2017.day5
   (:require [clojure.string :as str]))
 
-(defn move [v]
+(defn move [v calculate-new-offset]
   (loop [steps 0 current 0 jump-vector v]
     (let [jump-offset (get jump-vector current)
           next (+ current jump-offset)]
       (if (nil? (get jump-vector next))
         (inc steps)
-        (recur (inc steps) next (assoc jump-vector current (inc jump-offset))))))) 
+        (let [changed-offset (calculate-new-offset jump-offset)]
+          (recur (inc steps) next (assoc jump-vector current changed-offset)))))))
+
+(defn calculate-part2-offset [jump-offset]
+  (if (>= jump-offset 3) (dec jump-offset) (inc jump-offset)))
 
 (defn day5 [input]
-  (println "day 5 part 1:" (move (into [] (map #(Integer/parseInt %) (str/split-lines input))))))
+  (println "day 5 part 1:" (move (into [] (map #(Integer/parseInt %) (str/split-lines input))) #(inc %)))
+  (println "day 5 part 2:" (move (into [] (map #(Integer/parseInt %) (str/split-lines input))) calculate-part2-offset)))
