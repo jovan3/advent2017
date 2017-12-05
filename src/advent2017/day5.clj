7b9ef740b6db3b0cdb99927c460993d4e063dd8c
Yovan Naumovski <jovan3.14@gmail.com>
2017-12-05 21:00:37 +0200

    cleanup

--- src/advent2017/day5.clj
+++ src/advent2017/day5.clj
@@ -3,10 +3,11 @@
 
 (defn move [v]
   (loop [steps 0 current 0 jump-vector v]
-    (let [jump-offset (get jump-vector current)]
-      (if (nil? (get jump-vector (+ current jump-offset)))
+    (let [jump-offset (get jump-vector current)
+          next (+ current jump-offset)]
+      (if (nil? (get jump-vector next))
         (inc steps)
-        (recur (inc steps) (+ current jump-offset) (assoc jump-vector current (inc jump-offset))))))) 
+        (recur (inc steps) next (assoc jump-vector current (inc jump-offset))))))) 
 
 (defn day5 [input]
   (println "day 5 part 1:" (move (into [] (map #(Integer/parseInt %) (str/split-lines input))))))
